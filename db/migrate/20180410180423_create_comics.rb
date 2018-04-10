5ec35bb57addd68b4e2bd44342a3f50550a896f8
Tanya M Pritchett <tanyampritchett@Tanyas-MacBook-Pro.local>
2018-04-10 14:16:02 -0400

    added models

---
+++ db/migrate/20180410180423_create_comics.rb
@@ -0,0 +1,11 @@
+class CreateComics < ActiveRecord::Migration[5.1]
+  def change
+    create_table :comics do |t|
+      t.string :title
+      t.string :description
+      t.string :photo_url
+
+      t.timestamps
+    end
+  end
+end
