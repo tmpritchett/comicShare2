9731e7c1dc9afa9f87f5420a1914d58d2977529f
Tanya M Pritchett <tanyampritchett@Tanyas-MacBook-Pro.local>
2018-04-11 10:38:46 -0400

    added routes

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -1,2 +1,3 @@
 class Comic < ApplicationRecord
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
+# Rails.application.routes.draw do
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# end
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :users do
+      resources :comics do
+      resources :comments
+    end
+  end
 end
+end
+
+
